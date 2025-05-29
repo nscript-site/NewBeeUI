2db5f2fb6c40dfe979535e123ca8d47e20243137
HUFEI\hufei-m <hufei@geblab.com>
2025-05-29 17:33:19 +0800

    + 把 avalonia 相关的解决方案合并到一起

---
+++ demo/AvaloniaDemo/AvaloniaDemo/Views/MainView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaDemo.Views;
+
+public class MainView : BaseView
+{
+    protected override object Build()
+    {
+        return VStack(0, 0).Children([
+                TextBlock("Hello World!").Align(0, 0),
+                new ToggleSwitch(),
+                IconButton(SearchIcon.Instance),
+                IconButton(NStyles.MeterialIcons.SearchWebIcon.Instance),
+            ]);
+    }
+}
