b1daeaec8de3bdf1442a848fc047fc58f4b25498
hufei <hufei@geblab.com>
2025-11-18 18:19:30 +0800

    iOS 下启动时设置背景色，不然会闪一下子，影响体验

--- demo/NewBeeUI.Demo.iOS/AppDelegate.cs
+++ demo/NewBeeUI.Demo.iOS/AppDelegate.cs
@@ -2,6 +2,7 @@
 using Avalonia.iOS;
 
 using Foundation;
+using UIKit;
 
 namespace NewBeeUI.Demo.iOS;
 
@@ -13,7 +14,11 @@ public partial class AppDelegate : AvaloniaAppDelegate<App>
 {
     protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
     {
-        return base.CustomizeAppBuilder(builder)
-            .WithInterFont();
+        return base.CustomizeAppBuilder(builder).AfterSetup(_ =>
+        {
+            // 设置背景色，不然会是黑色的，闪一下子
+            if (this.Window != null)
+                this.Window.BackgroundColor = UIColor.White;
+        }).WithInterFont();
     }
 }
