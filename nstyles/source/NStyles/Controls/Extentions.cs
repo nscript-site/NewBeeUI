00b53518400d605a34b2602a58c40af95bab4483
hufei <hufei@geblab.com>
2026-05-05 15:15:54 +0800

    屏蔽 TextBox 的  style（在 avalonia 12 AOT 下有 bug）

--- nstyles/source/NStyles/Controls/Extentions.cs
+++ nstyles/source/NStyles/Controls/Extentions.cs
@@ -7,38 +7,38 @@ public static class Extentions
 {
     public static TextBox ListenIME(this TextBox textBox, Predicate<TextBox> predicate = null)
     {
-        if(predicate != null && predicate(textBox) == false)
-        {
-            return textBox; // 如果不满足条件，则直接返回
-        }
+        //if(predicate != null && predicate(textBox) == false)
+        //{
+        //    return textBox; // 如果不满足条件，则直接返回
+        //}
 
-        bool inited = false;
-        TextBlock? waterMark = null;
+        //bool inited = false;
+        //TextBlock? waterMark = null;
 
-        textBox.KeyDown += (s, e) =>
-        {
-            if (inited == false)
-            {
-                inited = true;
-                // 当按下键盘时，隐藏水印
-                foreach (var descendant in textBox.GetVisualDescendants())
-                {
-                    if (descendant is TextBlock tb && tb.Name == "watermark")
-                    {
-                        waterMark = tb;
-                    }
-                }
-            }
-            if(waterMark != null) waterMark.IsVisible = false;
-        };
+        //textBox.KeyDown += (s, e) =>
+        //{
+        //    if (inited == false)
+        //    {
+        //        inited = true;
+        //        // 当按下键盘时，隐藏水印
+        //        foreach (var descendant in textBox.GetVisualDescendants())
+        //        {
+        //            if (descendant is TextBlock tb && tb.Name == "watermark")
+        //            {
+        //                waterMark = tb;
+        //            }
+        //        }
+        //    }
+        //    if(waterMark != null) waterMark.IsVisible = false;
+        //};
 
-        textBox.LostFocus += (s, e) =>
-        {
-            if (waterMark != null)
-            {
-                waterMark.IsVisible = String.IsNullOrEmpty(textBox.Text);
-            }
-        };
+        //textBox.LostFocus += (s, e) =>
+        //{
+        //    if (waterMark != null)
+        //    {
+        //        waterMark.IsVisible = String.IsNullOrEmpty(textBox.Text);
+        //    }
+        //};
 
         return textBox;
     }
